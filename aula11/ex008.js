63076f82866395a3e41324ffecb66a60f7b593e7
Guilherme de Souza <112214045+guisouza-dev@users.noreply.github.com>
2022-09-14 21:31:15 -0300

    Introdução ás condições em HTML5

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+//console.log('Olá, Mundo!')
+
+let velocidade = 95
+if(velocidade > 80) { //Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Seu veículo chegou à ${velocidade}km/h`)
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+let país = 'Jamaica'
+if(país.toLowerCase() == 'brasil') {
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Seu país é ${país}, portanto, você não é Brasileiro!`)
+}
